228c29941e1957166ea81ad2f8aeac85df331289
Drew Dakin <dakindre4@gmail.com>
2018-04-16 00:01:56 -0400

    Working for BTS
    Need to fix LCV in order to get the function to run

--- csp.py
+++ csp.py
@@ -6,38 +6,58 @@ from collections import Counter
 class csp:
     def __init__(self, nput):
         self.nput = nput
-        self.board = {}
         self.alphaList = 'ABCDEFGHI'
         self.numbeList = '123456789'
+        
+        self.board = {}
+        self.createBoard()
+        
         self.constraints = []
-        self.neighbors = {}
-        self.boardSkeleton()
         self.createConstraints()
+        
+        self.neighbors = {}
         self.build_neighbors()
+
+        self.pruned = {}
         
         
 
-    def boardSkeleton(self):
+    ###########################################################################
+    # Create Empty Board with IDs
+    # Assign Domains to Empty Board
+    
+    def createBoard(self):
         self.emptyBoard=[]
         for x in self.alphaList:
             for y in self.numbeList:
                 self.emptyBoard.append(x+y)
-        self.createBoard()
+        self.assignBoard()
 
 
-    def createBoard(self):
+    def assignBoard(self):
         for x in range(0, len(self.emptyBoard)):
             if self.nput[x] == '0':
                 domain = list(range(1,10))
             else:
                 domain = self.nput[x]
                 domain = [int(domain)]
-            
             self.board.update({self.emptyBoard[x]: domain})
-        
+
+
+    def createPrune(self):
+        for x in range(0, len(self.emptyBoard)):
+            if self.nput[x] == '0':
+                domain = list()
+            else:
+                domain = self.nput[x]
+                domain = [int(domain)]
+            self.pruned.update({self.emptyBoard[x]: domain})
             
+        
     ####################################
-    # Fix
+    #
+
+    #Bad
     def createConstraints(self):
         blocks = (
             [self.combine(self.alphaList, number) for number in self.numbeList] +
@@ -50,6 +70,7 @@ class csp:
                 if [combination[0], combination[1]] not in self.constraints:
                     self.constraints.append([combination[0], combination[1]])
 
+    #Bad
     def build_neighbors(self):
 
         for x in self.board:
@@ -58,6 +79,7 @@ class csp:
                 if x == c[0]:
                     self.neighbors[x].append(c[1])
 
+    #Bad
     def permutate(self, iterable):
         result = list()
 
@@ -68,29 +90,133 @@ class csp:
 
         return result
 
+    #Bad
     def combine(self, alpha, beta):
         return [a + b for a in alpha for b in beta]
 
-    ###########################################################################
+    
 
-    def minimumRemainingVariable(self):
-        for key, value in self.board.items:
-            minV = 10
-            if len(value) > 1 and len(value) < minV:
-                minV = len(value)
-                minK = key
-            else: continue
-        return minK
 
-    def orderDomainValues(self, var, assignment):
+
+
+    ###################################################################
+    # Backtrack Functions:
+    # 1. MRV - returns key with least values in domain
+    # 2. LCV - returns sorted list of values from least occurences
+    # 3. consistant - returns bool true if value is not assigned to neighbor
+    # 4. forwardChecking - removes val from neighbor domain and creates inference
+
+    #Good
+    def MRV(self, assignment):
+        newBoard = {key: value for key, value in self.board.items() if key not in assignment}
+        for k in sorted(newBoard, key=lambda k: len(newBoard[k])):
+            return k
+
+    #Bad
+    def LCV(self, var):
+        neighbors = {key: value for key, value in self.board.items() if key in self.neighbors[var]}
         c = Counter()
-        for x in self.board.itervalues():
-            c.update(set(v))
+        for x in neighbors.values():
+            c.update(set(x))
         Counter(c.values())
+        print(neighbors)
+        print(c)
+        sort = sorted(c.values())
+        print(sort)
+        
+
+
+    
+
+
+
+    #Bad
+    def conflicts(self, var, val):
+
+        count = 0
+
+        for n in self.neighbors[var]:
+            if len(self.board[n]) > 1 and val in self.board[n]:
+                count += 1
+
+        return count
+    
+    
+
+
+
+    #Bad
+    def consistent(self, assignment, var, value):
+        consistent = True
+
+        for key, val in assignment.items():
+            if val == value and key in self.neighbors[var]:
+                consistent = False
+
+        return consistent
+
+    #Bad
+    def assign(self, var, value, assignment):
+
+        assignment[var] = value
+
+        self.forwardChecking(var, value, assignment)
+
+        
+    #Bad
+    def unassign(self, var, assignment):
+
+        if var in assignment:
+
+            for (D, v) in self.pruned[var]:
+                self.board[D].append(v)
+
+            self.pruned[var] = []
+
+            del assignment[var]
+
+    #Bad
+    def forwardChecking(self, var, value, assignment):
+        for neighbor in self.neighbors[var]:
+            if neighbor not in assignment:
+                if value in self.board[neighbor]:
+                    self.board[neighbor].remove(value)
+                    self.pruned[var].append((neighbor, value))
+
+
+    ###########################################################################
+    # Check If puzzle is solved for AC3
+    # Check if Assignment is complete
+    # Create Output String of Solved Puzzle
+
+    def solved(self):
+        return sum(len(v) for k, v in self.board.items()) == 81
+
+    def complete(self, assignment):
+        for x in self.emptyBoard:
+            if x in assignment:
+                continue
+            else: return False
+        return True
+
+    
+    def solutionStr(self):
+        solution = ''
+        for value in self.board.values():
+            solution = solution + str(value[0])
+        return solution
+
+        
+
+'''    def createAssignment(self):
+        for key, value in self.board.items():
+            if len(value) == 1:
+                self.assignment[key] = value[0]'''
+    
+    
             
 
-    unassigned = [v for v in sudoku.variables if v not in assignment]
-    return min(unassigned, key=lambda var: len(sudoku.domains[var]))
+    
             
 
         
--- driver_3.py
+++ driver_3.py
@@ -1,37 +1,113 @@
 import sys
 import argparse
-import csp
+import csp as c
+from collections import deque
+from copy import deepcopy
 
 
+###########################################################################
+# AC3 Algorithm
+#   Returns Success/Failure
+# Revision function to remove unwanted values in domain
+#   Returns True if value removed
 
-def solutionStr(csp):
-        solution = ''
-        for key, value in csp.items():
-            solution = solution + str(value[0])
-        return solution
+def AC3(csp):
+    queue = deque()
+    for x in csp.constraints:
+       queue.append(x)
+       
+    while queue:
+        xi, xj = queue.popleft()
+        if revise(csp, str(xi), str(xj)):
+            if len(csp.board[xi]) == 0:
+                return False
+            for xk in csp.neighbors[xi]:
+                if xk != xi:
+                    queue.append([xk, xi])
+
+    return True
+
+
     
-    
+def revise(csp, xi, xj):
+    revised = False
+    for x in csp.board[xi]:
+        count = 0
+        for y in csp.board[xj]:
+            if x != y:
+                count += count+1
+        if count == 0:
+            csp.board[xi].remove(x)
+            revised = True
+
+    return revised
+
+
+
+###########################################################################
+# backtracking - assigning values to keys in assignment
+#   Return: Assignment/False
+
+
+def backtrack(assignment, csp):
+
+    if csp.complete(assignment):
+        return assignment
+
+    var = csp.MRV(assignment)
+    for value in csp.LCV(var):
+
+        if csp.consistent(assignment, var, value):
+
+            csp.assign(var, value, assignment)
+
+            result = backtrack(assignment, csp)
+            if result:
+                return result
+
+            csp.unassign(var, assignment)
+
+    return False           
+
+
+
+
+
+###########################################################################
+# Write Output String to file
+# Main Function
+
 def writeOutput(output):
     with open('output.txt', 'a') as myfile:
         myfile.write(output + '\n')
-        
-
-    
-def RunCSP(csp):
-    assignment = AC3(csp)
-    if assignment:
-        return csp.solutionStr() + ' AC3'
-    assignment = BTS(csp)
-    return ' BTS'
-
 
 
 def main():
+    #Import Puzzle to be solved
     parser = argparse.ArgumentParser()
     parser.add_argument('b', type=str)
     args = parser.parse_args()
-    board = csp(args.b)
-    writeOutput(RunCSP(board))
+
+    #Create Initial State Board
+    csp = c.csp(args.b)
+
+    #Run AC3/BackTrack
+    if(AC3(csp)):
+        if csp.solved():
+            writeOutput(csp.solutionStr() + ' AC3')
+
+        else:
+            csp.createPrune()
+            n = {key: value[0] for key, value in csp.board.items() if len(value) == 1}
+            assignment = backtrack(n, csp)
+            
+            if isinstance(assignment, dict):
+                for key, value in assignment.items():
+                    csp.board[key] = [value]
+                writeOutput(csp.solutionStr() + ' BTS')
+
+        
+
             
 
 if __name__ == "__main__":
